40dff0cdcf024946ab764d8d81d34f357d83720b
helioz2000 <helioz2000@gmail.com>
2018-12-10 23:26:18 +1100

    Documentation for interrupt code

--- IR_38kHz_TX/IR_38kHz_TX.ino
+++ IR_38kHz_TX/IR_38kHz_TX.ino
@@ -17,36 +17,72 @@ yes,/*
 #define SYSCLOCK 16000000  // main system clock (Hz)
 #define PULSECLOCK 38000  // Hz
 
-#define IROUT 11
+#define IROUT 11    // Interrupt output pin (do not change OC2 = 11, fixed)
 
 uint8_t timer2top(unsigned long freq) ;
 
 void setup() {
  Serial.begin(9600) ;
 
+ // Set timer2 to produce wave form
+ /*
+  * Compare Output Mode non PWM
+  * Toggle OC2A on compare match
+  * COM2A0 =1; COM2A1 =0
+  */ 
  cbi(TCCR2A,COM2A1) ; // connect OC2A (COM2A0 = 1)
  sbi(TCCR2A,COM2A0) ;
- 
+
+ /*
+  * Waveform Generation Mode
+  * Mode of operation = CTC - Clear Timer on Compare Match
+  * TOP (max counter value) = OCRA register
+  * Update of OCR02 = Immediate
+  * TOV FLag Set on = MAX
+  * WGM22 =0; WGM21=1; WGM20-0
+  */
  cbi(TCCR2B,WGM22) ;  // CTC mode for TIMER2
  sbi(TCCR2A,WGM21) ;
  cbi(TCCR2A,WGM20) ;
  
  TCNT2 = 0 ;
- 
+
+ /*
+  * Asyncronous Status register
+  * Asyncronous Timer clocked from I/O clock (clkI/O)
+  * AS2 =0;
+  */
  cbi(ASSR,AS2) ;  // use system clock for timer 2
- 
+
+  /*
+   * The maximum value, to be overwritten further on
+   */
  OCR2A = 255 ;   // set TOP to 255 for now
- 
+
+ /*
+  * Clock Set
+  * Prescale: 1 (no prescale)
+  * CS20 =1; CS21 =0; CS22 =0;
+  */
  cbi(TCCR2B,CS22) ;  // TIMER2 prescale = 1
  cbi(TCCR2B,CS21) ;
  sbi(TCCR2B,CS20) ;
- 
+
+ /*
+  * Force Output Compare A + B
+  * clear
+  */
  cbi(TCCR2B,FOC2A) ;  // clear forced output compare bits
  cbi(TCCR2B,FOC2B) ;
 
- pinMode(IROUT, OUTPUT) ;  // set OC2A to OUPUT  
- OCR2A = timer2top(PULSECLOCK) ; 
- sei() ;
+ pinMode(IROUT, OUTPUT) ;  // set OC2A to OUPUT
+
+ // set calculate TOP value
+ OCR2A = timer2top(PULSECLOCK) ;
+
+ // enable interrupts
+ sei();
+ 
 }
 
 // main loop
